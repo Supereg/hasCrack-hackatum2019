776fba8dd614247b57f5b8b13ecec0d67e5ed218
Supereg <mail@anderl-bauer.de>
2019-11-24 01:57:39 +0100

    Change default port

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -12,7 +12,7 @@ const debug = createDebug('hackatum2019-ui:server');
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
 app.set('env', "development"); // TODO remove
 
